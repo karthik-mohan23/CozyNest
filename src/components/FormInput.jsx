f921ceb9d774d7f397b672c5f790c66c8ad2e675
karthik-mohan23 <karthikmohan2310@gmail.com>
2023-09-07 15:02:01 +0530

    create FormInput component

---
+++ src/components/FormInput.jsx
@@ -0,0 +1,18 @@
+const FormInput = (props) => {
+  const { name, defaultValue, type, label } = props;
+
+  return (
+    <div className="form-control ">
+      <label className="label">
+        <span className="label-text">{label}</span>
+      </label>
+      <input
+        type={type}
+        name={name}
+        defaultValue={defaultValue}
+        className="input input-bordered "
+      />
+    </div>
+  );
+};
+export default FormInput;
